bbd5aeddbe0579c4a8e5da8b7fb0d6188287dc29
webwaste <hiranmay.das@niser.ac.in>
2022-07-26 16:09:24 +0530

    compiler updated

--- compiler.sh
+++ compiler.sh
@@ -2,4 +2,5 @@ g++ -ljsoncpp cpp_program_files/rand_pi_flux_generator.cpp -o cpp_executables/ra
 g++ cpp_program_files/gauge_fixer.cpp -o cpp_executables/gauge_fixer
 #g++ cpp_program_files/ham_constructor.cpp -o cpp_executables/ham_constructor
 g++ -ljsoncpp cpp_program_files/eigval_generator.cpp -llapack -o cpp_executables/eigval_generator
+g++ -ljsoncpp cpp_program_files/eigstate_generator.cpp -llapack -o cpp_executables/eigstate_generator
 #g++ cpp_program_files/dos.cpp -o cpp_executables/dos
