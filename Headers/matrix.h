96a2d0d62f12bc3d54e278fad9e5eac8d3aa8717
webwaste <hiranmay.das@niser.ac.in>
2022-07-26 16:12:28 +0530

    matrix class updated

--- Headers/matrix.h
+++ Headers/matrix.h
@@ -1,7 +1,14 @@
 #include<iostream>
 #include<cstring>
 #include<fstream>
+#include<cblas.h>
+#include<vector>
 using namespace std; 
+
+extern "C"{
+	void dsyev_(char* JOBZ, char* UPLO, int* N, double* A, int* LDA, double* W, double* WORK, int* LWORK, int* INFO);
+}
+
 
 class matrix{
 	public:
@@ -9,10 +16,53 @@ class matrix{
 	int col; 
 	double* array = (double*) malloc(row*col*sizeof(double));
 
-	matrix(int i=1, int j=1):row(i),col(j) {}
-	double elmnt(int r, int c){
+	matrix(int r=1, int c=1):row(r),col(c) {
+	for(int i=0; i<r*c; i++){
+		array[i]=0;
+	}
+	}
+
+	double get(int r, int c){
 		return array[r*col + c];
 	}
+
+	void set(double a, int r, int c){
+		array[r*col + c] = a;
+	}
+
+
+	double* eig_val(){
+		char JOBZ = 'N'; //computes only eigen values.
+		char UPLO = 'U'; //upper triagular part of A is considered. 
+		int N = row;
+		int LDA = row;
+		double* W = (double*) malloc(row*sizeof(double));
+		int LWORK = row*col;
+		double* WORK = (double*) malloc(LWORK*sizeof(double));
+		int INFO = 0;
+
+		dsyev_(&JOBZ, &UPLO, &N, array, &LDA, W, WORK, &LWORK, &INFO);
+//		vector<double> eigv(W, W + sizeof(W)/sizeof(W[0]));
+//		return eigv;
+		return W;
+	}
+
+	double* eig_vec(){
+		char JOBZ = 'V'; //computes eigenvectors.
+		char UPLO = 'U'; //upper triagular part of A is considered. 
+		int N = row;
+		int LDA = row;
+		double* W = (double*) malloc(row*sizeof(double));
+		int LWORK = 3*row*col;
+		double* WORK = (double*) malloc(LWORK*sizeof(double));
+		int INFO = 0;
+
+		dsyev_(&JOBZ, &UPLO, &N, array, &LDA, W, WORK, &LWORK, &INFO);
+//		vector<double> eigv(W, W + sizeof(W)/sizeof(W[0]));
+//		return eigv;
+		return array;
+	}
+	
 
 	void read(string name){
 		ifstream ifile(name);
